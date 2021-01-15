a51cec0a692fbd1a3ab46d090320c3ad74e1992f
arielvergarautn <arie@est.frba.utn.edu.ar>
2021-01-15 06:33:29 -0300

    icons and stars

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -8,6 +8,16 @@ import { useHistory } from 'react-router-dom'
 //Util
 import { smallImage } from '../util'
 
+//Icons
+import apple from '../img/apple.svg'
+import playstation from '../img/playstation.svg'
+import xbox from '../img/xbox.svg'
+import nintendo from '../img/nintendo.svg'
+import steam from '../img/steam.svg'
+import gamepad from '../img/gamepad.svg'
+import starEmpty from '../img/star-empty.png'
+import starFull from '../img/star-full.png'
+
 const GameDetails = ({ pathId }) => {
 
     const { screenShot, game, isLoading } = useSelector(state => state.gameDetails)
@@ -21,6 +31,44 @@ const GameDetails = ({ pathId }) => {
         }
     }
 
+    const getPlatformImages = (platform) => {
+
+        if (platform.toLowerCase().includes('playstation')) {
+            return playstation;
+        }
+        else if (platform.toLowerCase().includes('xbox')) {
+            return xbox;
+        }
+        else if (platform.toLowerCase().includes('pc')) {
+            return steam;
+        }
+        else if (platform.toLowerCase().includes('nintendo')) {
+            return nintendo;
+        }
+        else if (platform.toLowerCase().includes('ios')) {
+            return apple;
+        }
+        else {
+            return gamepad;
+        }
+    }
+
+    const getStars = () => {
+        const stars = [];
+        const rating = Math.floor(game.rating);
+
+        for (let i = 1; i <= 5; i++) {
+            if (i <= rating)
+                stars.push(<img alt='start' src={starFull} key={i} />)
+            else
+                stars.push(<img alt='start' src={starEmpty} key={i} />)
+
+        }
+
+        return stars;
+    }
+
+
 
     return (
         <>
@@ -32,13 +80,16 @@ const GameDetails = ({ pathId }) => {
                             <div className="rating">
                                 <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
                                 <p>Rating: {game.rating}</p>
+                                {
+                                    getStars()
+                                }
                             </div>
                             <div className="info">
                                 <h3>Platforms</h3>
                                 <div className="platforms">
                                     {
                                         game.platforms.map(platform => (
-                                            <h3 key={platform.platform.id}>{platform.platform.name}</h3>
+                                            <img key={platform.platform.id} title={platform.platform.name} src={getPlatformImages(platform.platform.name)} alt={platform.platform.name} />
                                         ))
                                     }
                                 </div>
